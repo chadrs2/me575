f5f76819ef03c10b435d5415ff3d3108e798f1d9
Chad Samuelson <crsamuelson87@gmail.com>
2022-01-31 16:21:29 -0700

    adjusted hw2 layout

---
+++ hw2/hw2.py
@@ -0,0 +1,8 @@
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+import numpy as np
+from math import sqrt
+from uncon import uncon, quadratic, rosenbrock, brachistochrone 
+
+if __name__ == '__main__':
+    print("Hello World!")
