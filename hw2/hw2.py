06e383eb85508d4012c6d9b4540eef0189ff31eb
Chad Samuelson <crsamuelson87@gmail.com>
2022-02-01 08:08:50 -0700

    Quadratic function working

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,15 +1,111 @@
+from threading import local
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 import numpy as np
 from math import sqrt
-from uncon import uncon, quadratic, rosenbrock, brachistochrone 
+from uncon import uncon
+
+global f_calls
+
+def quadratic(x):
+    beta = 3./2.
+    f = x[0]**2 + x[1]**2 - beta * x[0] * x[1]
+    df = np.array([
+        2 * x[0] - beta * x[1],
+        2 * x[1] - beta * x[0]
+        ])
+    # global f_calls 
+    # f_calls += 1
+    
+    return f, df
+
+def quad_helper(x):
+    f, df = quadratic(x)
+    return f
+
+def rosenbrock(x):
+    Sigma = 0
+    for i in range(len(x)-1):
+        Sigma += 100 * (x[i+1] - x[i]**2)**2 + (1 - x[i])**2
+    dSigma = np.zeros(len(x))
+    for i in range(len(x)):
+        if i == 0:
+            dSigma[i] = -400 * x[i] * (x[i+1] - x[i]**2) - 2 * (1 - x[i])
+        elif i == len(x)-1:
+            dSigma[i] = 200 * (x[i] - x[i-1]**2)
+        else:
+            dSigma[i] = 200 * (x[i] - x[i-1]**2) - 400 * x[i] * (x[i+1] - x[i]**2) - 2 * (1 - x[i])
+    return Sigma, dSigma
+
+def rosenbrock_helper(x):
+    Sigma, dSigma = rosenbrock(x)
+    return Sigma
+
+def brachistochrone(yint):
+    """ brachistochrone problem.
+        
+    Parameters
+    ----------
+    yint : a vector of y location for all the interior points
+
+    Outputs
+    -------
+    T : scalar proportion to the total time it takes the bead to traverse
+        the wire
+    dT : dT/dyint the derivatives of T w.r.t. each yint.
+    """
+     
+    # friction
+    mu = 0.3
+    
+    # setup discretization
+    h = 1.0
+    y = np.concatenate([[h], yint, [0]])
+    n = len(y)
+    x = np.linspace(0.0, 1.0, n)
+    
+    # initialize
+    T = 0.0
+    dT = np.zeros(n-2)
+    
+    for i in range(n-1):
+        ds = sqrt((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)
+        vbar = sqrt(h - y[i+1] - mu*x[i+1]) + sqrt(h - y[i] - mu*x[i])
+        T += ds/vbar
+        
+        # gradient
+        if i > 0:
+            dsdyi = -(y[i+1] - y[i])/ds
+            dvdyi = -0.5/sqrt(h - y[i] - mu*x[i])
+            dtdyi = (vbar*dsdyi - ds*dvdyi)/(vbar**2)
+            dT[i-1] += dtdyi
+        
+        if i < n-2:
+            dsdyip = (y[i+1] - y[i])/ds
+            dvdyip = -0.5/sqrt(h - y[i+1] - mu*x[i+1])
+            dtdyip = (vbar*dsdyip - ds*dvdyip)/(vbar**2)
+            dT[i] += dtdyip
+         
+    return T, dT
+
 
 if __name__ == '__main__':
-    x0 = np.ones((2,1))*50
+    print("*************** QUADRATIC OPTIMIZATION ***************")
+    x0 = np.ones((2,))
+    print("Initial X0:",x0)
+    # f_calls = 0
     tau = 1e-6
-
+    print("--------------------")
     # Global minimum for quadratic should be: x* = [0,0], f(x*) = 0
     xopt, fopt = uncon(quadratic,x0,tau)
-    print("SOLUTION:")
+    # print("Func calls:",f_calls)
+    print("MY SOLUTION:")
     print("X:",xopt)
     print("f:",fopt)
+    print("--------------------")
+
+    X = minimize(quad_helper,x0)
+    print("THEIR SOLUTION:")
+    print(X)
+
+    print("*************** ROSENBROCK OPTIMIZATION ***************")
--- hw2/uncon.py
+++ hw2/uncon.py
@@ -1,76 +1,6 @@
 import numpy as np
 from math import sqrt
 
-def quadratic(x):
-    beta = 3./2.
-    f = x[0,0]**2 + x[1,0]**2 - beta * x[0,0] * x[1,0]
-    df = np.array([[
-        2 * x[0,0] - beta * x[1,0],
-        2 * x[1,0] - beta * x[0,0]
-        ]]).T
-    return f, df
-
-def rosenbrock(x):
-    Sigma = 0
-    for i in range(len(x)-1):
-        Sigma += 100 * (x[i+1] - x[i]**2)**2 + (1 - x[i])**2
-    dSigma = np.zeros(len(x))
-    for i in range(len(x)):
-        if i == 0:
-            dSigma[i] = -400 * x[i] * (x[i+1] - x[i]**2) - 2 * (1 - x[i])
-        elif i == len(x)-1:
-            dSigma[i] = 200 * (x[i] - x[i-1]**2)
-        else:
-            dSigma[i] = 200 * (x[i] - x[i-1]**2) - 400 * x[i] * (x[i+1] - x[i]**2) - 2 * (1 - x[i])
-    return Sigma, dSigma
-
-def brachistochrone(yint):
-    """ brachistochrone problem.
-        
-    Parameters
-    ----------
-    yint : a vector of y location for all the interior points
-
-    Outputs
-    -------
-    T : scalar proportion to the total time it takes the bead to traverse
-        the wire
-    dT : dT/dyint the derivatives of T w.r.t. each yint.
-    """
-     
-    # friction
-    mu = 0.3
-    
-    # setup discretization
-    h = 1.0
-    y = np.concatenate([[h], yint, [0]])
-    n = len(y)
-    x = np.linspace(0.0, 1.0, n)
-    
-    # initialize
-    T = 0.0
-    dT = np.zeros(n-2)
-    
-    for i in range(n-1):
-        ds = sqrt((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)
-        vbar = sqrt(h - y[i+1] - mu*x[i+1]) + sqrt(h - y[i] - mu*x[i])
-        T += ds/vbar
-        
-        # gradient
-        if i > 0:
-            dsdyi = -(y[i+1] - y[i])/ds
-            dvdyi = -0.5/sqrt(h - y[i] - mu*x[i])
-            dtdyi = (vbar*dsdyi - ds*dvdyi)/(vbar**2)
-            dT[i-1] += dtdyi
-        
-        if i < n-2:
-            dsdyip = (y[i+1] - y[i])/ds
-            dvdyip = -0.5/sqrt(h - y[i+1] - mu*x[i+1])
-            dtdyip = (vbar*dsdyip - ds*dvdyip)/(vbar**2)
-            dT[i] += dtdyip
-         
-    return T, dT
-
 def conjugate_grad(df,df_prev,p_prev):
     beta = (df.T @ df) / (df_prev.T @ df_prev)
     p = - df + beta * p_prev
@@ -140,11 +70,12 @@ def uncon(func, x0, tau):
         alpha = backtracking(func, x, alpha, p)
 
         x = x + alpha * p
-
+        
         f_prev = f
         df_prev = df
         p_prev = p 
-    
+        k += 1
+    print("It took k =",k,"operations to converge")
     xopt = x
     fopt = f
     return xopt, fopt
